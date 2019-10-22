052b8dbde8841246735c833f3d988804a26a2562
devjosegomez <devjosegomez@gmail.com>
2019-10-22 11:17:30 -0500

    Initial commit

---
+++ src/com/exercise18collections/app/Collections.java
@@ -0,0 +1,10 @@
+package com.exercise18collections.app;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/exercise18linkedlist/app/LinkedList.java
@@ -0,0 +1,10 @@
+package com.exercise18linkedlist.app;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
