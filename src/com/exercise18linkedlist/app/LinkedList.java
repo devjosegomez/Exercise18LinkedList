203c5f4a26d42f1cde899825a7e8175a1c579bd5
devjosegomez <devjosegomez@gmail.com>
2019-10-22 13:13:18 -0500

    Example of iterator in collections

--- src/com/exercise18linkedlist/app/LinkedList.java
+++ src/com/exercise18linkedlist/app/LinkedList.java
@@ -5,17 +5,31 @@ import java.util.Queue;
 import java.util.Stack;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.Iterator;
 
 public class LinkedList {
 
 	public static void main(String[] args) {
 		
 		//LinkedList<Byte> myList = new LinkedList<Byte>();
-		LinkedList myList1 = new LinkedList();
+		LinkedList miListita = new LinkedList();
 		ArrayList<Short> miArrayList = new ArrayList<Short>();
 		Stack<Integer> miPilita = new Stack<Integer>();
 		PriorityQueue<Double> miColita = new PriorityQueue<Double>();
 		HashMap<Integer, String> miDiccionario = new HashMap<Integer, String>();
+		
+		//miListita.add((byte)5);
+		//miListita.set(1, (byte) 2);
+		
+		Iterator miIterator = miArrayList.iterator();
+		while(miIterator.hasNext()) {
+			System.out.println(miIterator.next());
+		}
+		
+		for(Short mynumber : miArrayList){
+			System.out.println(mynumber);
+		}
+		
 	}
 
 }
