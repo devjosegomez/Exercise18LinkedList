4885b8c188eba36a1025b4df5e98fd5d25a2c18b
devjosegomez <devjosegomez@gmail.com>
2019-10-22 12:18:13 -0500

    Example collections generics

--- src/com/exercise18linkedlist/app/LinkedList.java
+++ src/com/exercise18linkedlist/app/LinkedList.java
@@ -1,10 +1,21 @@
 package com.exercise18linkedlist.app;
+import java.util.ArrayList;
+//import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class LinkedList {
 
 	public static void main(String[] args) {
 		
-
+		//LinkedList<Byte> myList = new LinkedList<Byte>();
+		LinkedList myList1 = new LinkedList();
+		ArrayList<Short> miArrayList = new ArrayList<Short>();
+		Stack<Integer> miPilita = new Stack<Integer>();
+		PriorityQueue<Double> miColita = new PriorityQueue<Double>();
+		HashMap<Integer, String> miDiccionario = new HashMap<Integer, String>();
 	}
 
 }
